07f7abbaed7697cd7dcc5dbc26916722e4b9d067
Mike Gerard Suico <mgsuico.mentorspire@gmail.com>
2024-11-19 16:20:38 +0800

    Added schedule feeding

--- app/Livewire/Pages/FeedingSchedule.php
+++ app/Livewire/Pages/FeedingSchedule.php
@@ -2,10 +2,153 @@
 
 namespace App\Livewire\Pages;
 
+use Filament\Notifications\Notification;
 use Livewire\Component;
+use Kreait\Firebase\Contract\Database;
 
 class FeedingSchedule extends Component
 {
+    protected Database $database;
+    public $timeOne;
+    public $timeTwo;
+    public $timeThree;
+    public $timeFour;
+    public $timeFive;
+
+    public function mount(Database $database){
+        $this->database = $database;
+        $this->getFeedingSchedules();
+    }
+
+    public function getFeedingSchedules()
+    {
+        try {
+            // Fetch multiple references
+            $timeOneReference = $this->database->getReference('/Schedule/TimeOne');
+            $timeTwoReference = $this->database->getReference('/Schedule/TimeTwo');
+            $timeThreeReference = $this->database->getReference('/Schedule/TimeThree');
+            $timeFourReference = $this->database->getReference('/Schedule/TimeFour');
+            $timeFiveReference = $this->database->getReference('/Schedule/TimeFive');
+
+            $rawTimeOne = $timeOneReference->getValue();
+            $rawTimeTwo = $timeTwoReference->getValue();
+            $rawTimeThree = $timeThreeReference->getValue();
+            $rawTimeFour = $timeFourReference->getValue();
+            $rawTimeFive = $timeFiveReference->getValue();
+
+            // Convert raw values to the desired format (e.g., "06:00")
+            $this->timeOne = sprintf('%02d:00', $rawTimeOne);
+            $this->timeTwo = sprintf('%02d:00', $rawTimeTwo);
+            $this->timeThree = sprintf('%02d:00', $rawTimeThree);
+            $this->timeFour = sprintf('%02d:00', $rawTimeFour);
+            $this->timeFive = sprintf('%02d:00', $rawTimeFive);
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error retrieving schedules: ' . $e->getMessage()], 500);
+        }
+    }
+
+
+    public function editFeedingScheduleOne(Database $database)
+    {
+        $this->database = $database;
+        try {
+            $rawTime = (int)explode(':', $this->timeOne)[0];
+           
+            $reference = $this->database->getReference('/Schedule/TimeOne');
+            $reference->set($rawTime); 
+
+            Notification::make()
+                ->title('Success!')
+                ->body('Schedule has been saved!')
+                ->success()
+                ->send();
+
+        } catch (\Exception $e) {
+            session()->flash('error', 'Error updating schedule: ' . $e->getMessage());
+        }
+    }
+
+    public function editFeedingScheduleTwo(Database $database)
+    {
+        $this->database = $database;
+        try {
+            $rawTime = (int)explode(':', $this->timeTwo)[0];
+           
+            $reference = $this->database->getReference('/Schedule/TimeTwo');
+            $reference->set($rawTime); 
+
+            Notification::make()
+                ->title('Success!')
+                ->body('Schedule has been saved!')
+                ->success()
+                ->send();
+
+        } catch (\Exception $e) {
+            session()->flash('error', 'Error updating schedule: ' . $e->getMessage());
+        }
+    }
+
+    public function editFeedingScheduleThree(Database $database)
+    {
+        $this->database = $database;
+        try {
+            $rawTime = (int)explode(':', $this->timeThree)[0];
+           
+            $reference = $this->database->getReference('/Schedule/TimeThree');
+            $reference->set($rawTime); 
+
+            Notification::make()
+                ->title('Success!')
+                ->body('Schedule has been saved!')
+                ->success()
+                ->send();
+
+        } catch (\Exception $e) {
+            session()->flash('error', 'Error updating schedule: ' . $e->getMessage());
+        }
+    }
+
+    public function editFeedingScheduleFour(Database $database)
+    {
+        $this->database = $database;
+        try {
+            $rawTime = (int)explode(':', $this->timeFour)[0];
+           
+            $reference = $this->database->getReference('/Schedule/TimeFour');
+            $reference->set($rawTime); 
+
+            Notification::make()
+                ->title('Success!')
+                ->body('Schedule has been saved!')
+                ->success()
+                ->send();
+
+        } catch (\Exception $e) {
+            session()->flash('error', 'Error updating schedule: ' . $e->getMessage());
+        }
+    }
+
+    public function editFeedingScheduleFive(Database $database)
+    {
+        $this->database = $database;
+        try {
+            $rawTime = (int)explode(':', $this->timeFive)[0];
+           
+            $reference = $this->database->getReference('/Schedule/TimeFive');
+            $reference->set($rawTime); 
+
+            Notification::make()
+                ->title('Success!')
+                ->body('Schedule has been saved!')
+                ->success()
+                ->send();
+
+        } catch (\Exception $e) {
+            session()->flash('error', 'Error updating schedule: ' . $e->getMessage());
+        }
+    }
+
     public function render()
     {
         return view('livewire.pages.feeding-schedule');
--- resources/views/livewire/pages/feeding-schedule.blade.php
+++ resources/views/livewire/pages/feeding-schedule.blade.php
@@ -1,3 +1,108 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="flex flex-col lg:flex-row gap-3">
+        <div class="flex flex-row w-full justify-center gap-2 mt-3">
+            <div class="w-full flex flex-col justify-between bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                <div class="mb-3">
+                    <h1 class="text-sm italic">Current:</h1>
+                    <h2 class="text-center py-2 text-lg font-bold">{{ $timeOne }}</h2>
+                </div>
+                <hr class="border-blue-500 mb-3">
+                <x-time-picker
+                    label="Select Time"
+                    placeholder="22:30"
+                    format="24"
+                    interval="60"
+                    wire:model.defer="timeOne"
+                />
+                <button wire:click="editFeedingScheduleOne" class="py-2 px-3 flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none mt-3">
+                    Apply
+                </button>
+                
+            </div>
+        </div>
+
+        <div class="flex flex-row w-full justify-center gap-2 mt-3">
+            <div class="w-full flex flex-col justify-between bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                <div class="mb-3">
+                    <h1 class="text-sm italic">Current:</h1>
+                    <h2 class="text-center py-2 text-lg font-bold">{{ $timeTwo }}</h2>
+                </div>
+                <hr class="border-blue-500 mb-3">
+                <x-time-picker
+                    label="Select Time"
+                    placeholder="22:30"
+                    format="24"
+                    interval="60"
+                    wire:model.defer="timeTwo"
+                />
+                <button wire:click="editFeedingScheduleTwo" class="py-2 px-3 flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none mt-3">
+                    Apply
+                </button>
+                
+            </div>
+        </div>
+
+        <div class="flex flex-row w-full justify-center gap-2 mt-3">
+            <div class="w-full flex flex-col justify-between bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                <div class="mb-3">
+                    <h1 class="text-sm italic">Current:</h1>
+                    <h2 class="text-center py-2 text-lg font-bold">{{ $timeThree }}</h2>
+                </div>
+                <hr class="border-blue-500 mb-3">
+                <x-time-picker
+                    label="Select Time"
+                    placeholder="22:30"
+                    format="24"
+                    interval="60"
+                    wire:model.defer="timeThree"
+                />
+                <button wire:click="editFeedingScheduleThree" class="py-2 px-3 flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none mt-3">
+                    Apply
+                </button>
+                
+            </div>
+        </div>
+
+        <div class="flex flex-row w-full justify-center gap-2 mt-3">
+            <div class="w-full flex flex-col justify-between bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                <div class="mb-3">
+                    <h1 class="text-sm italic">Current:</h1>
+                    <h2 class="text-center py-2 text-lg font-bold">{{ $timeFour }}</h2>
+                </div>
+                <hr class="border-blue-500 mb-3">
+                <x-time-picker
+                    label="Select Time"
+                    placeholder="22:30"
+                    format="24"
+                    interval="60"
+                    wire:model.defer="timeFour"
+                />
+                <button wire:click="editFeedingScheduleFour" class="py-2 px-3 flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none mt-3">
+                    Apply
+                </button>
+                
+            </div>
+        </div>
+
+        <div class="flex flex-row w-full justify-center gap-2 mt-3">
+            <div class="w-full flex flex-col justify-between bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                <div class="mb-3">
+                    <h1 class="text-sm italic">Current:</h1>
+                    <h2 class="text-center py-2 text-lg font-bold">{{ $timeFive }}</h2>
+                </div>
+                <hr class="border-blue-500 mb-3">
+                <x-time-picker
+                    label="Select Time"
+                    placeholder="22:30"
+                    format="24"
+                    interval="60"
+                    wire:model.defer="timeFive"
+                />
+                <button wire:click="editFeedingScheduleFive" class="py-2 px-3 flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none mt-3">
+                    Apply
+                </button>
+                
+            </div>
+        </div>
+    </div>
 </div>
